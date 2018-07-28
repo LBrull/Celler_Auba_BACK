ff5e1fb9178382b4bc31f2c3ca028b2bac1b51a6
Laura Brull Luelmo <laura.brull@est.fib.upc.edu>
2018-07-28 10:51:32 +0200

    generate tokens

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/celler_aubarcaDB'
+    db: process.env.MONGODB || 'mongodb://localhost:27017/celler_aubarcaDB',
+    SECRET_TOKEN: 'K0a64f021763a2b1e4315569b361096c387ab77S'
 }
